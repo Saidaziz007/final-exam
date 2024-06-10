42f7d79c81bd9bc94a40a466c1c890c6ed136c76
Saidaziz007 <saidaminovvs@gmail.com>
2024-06-10 06:51:46 +0500

    first commit

--- src/pages/not-found/Notfound.jsx
+++ src/pages/not-found/Notfound.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Notfound.css";
 
 const Notfound = () => {
-  return <div>Notfound</div>;
+  return (
+    <div className="notfound">
+      <div className="container">
+        <div className="notfound-all">
+          <h1>404</h1>
+          <p>Похоже, ничего не нашлось :(</p>
+          <Link to={"/"}>
+            <button>На главную</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Notfound;
